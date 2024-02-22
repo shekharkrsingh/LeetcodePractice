270deba7818b0b16ca6c80a41a807c763a056123
Shekhar Singh <107613589+shekharkrsingh@users.noreply.github.com>
2024-02-22 05:52:33 +0530

    Time: 150 ms (43.5%), Space: 72.4 MB (21.11%) - LeetHub

---
+++ 0997-find-the-town-judge/0997-find-the-town-judge.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int findJudge(int N, vector<vector<int>>& trust) {
+         vector<int> in(N), out(N);
+        
+        for(vector<int> edge : trust){
+            //1-based -> 0-based
+            int from = edge[0]-1, to = edge[1]-1;
+            out[from]++;
+            in[to]++;
+        }
+        
+        for(int i = 0; i < N; i++){
+            if(in[i] == N-1 && out[i] == 0){
+                //0-based -> 1-based
+                return i+1;
+            }
+        }
+        
+        return -1;
+    }
+};
